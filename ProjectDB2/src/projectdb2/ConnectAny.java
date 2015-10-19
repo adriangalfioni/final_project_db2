40139156a2170a66afdfd1d95a41883903e67acf
Adrian <adrian.galfioni@gmail.com>
2015-10-19 16:27:48 -0300

    Table class created

--- ProjectDB2/src/projectdb2/ConnectAny.java
+++ ProjectDB2/src/projectdb2/ConnectAny.java
@@ -5,12 +5,39 @@
  */
 package projectdb2;
 
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  *
  * @author adrian
  */
 public abstract class ConnectAny {
     
+    public abstract Connection connect(String host, String dbName, String user, String pass); 
+    
+    public HashMap<String,String> getConnectionInfo(){
+        String host, dbName, user, pass;
+        HashMap<String, String> connectionInfo = new HashMap<>();
+
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("Ingrese el nombre de Host");
+        host = sc.nextLine();
+        System.out.println("Ingrese el nombre de la Base de Datos");
+        dbName = sc.nextLine();
+        System.out.println("Ingrese el usuario");
+        user = sc.nextLine();
+        System.out.println("Ingrese la contraseña");
+        pass = sc.nextLine();
+        
+        connectionInfo.put("host", host);
+        connectionInfo.put("dbname", dbName);
+        connectionInfo.put("user", user);
+        connectionInfo.put("pass", pass);
+        
+        return connectionInfo;
+    }
     
 }
---
+++ ProjectDB2/src/projectdb2/Table.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectdb2;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author adrian
+ */
+public class Table {
+    
+    private String name;
+    private LinkedList<String[]> columns;
+    private LinkedList<String> pks;
+    private LinkedList<String> fks;
+    private LinkedList<String> uqks;
+    private LinkedList<String> triggers;
+    private LinkedList<String[]> indexs;
+
+    public Table() {
+        name = null;
+        columns = new LinkedList<String[]>();
+        pks = new LinkedList<String>();
+        fks = new LinkedList<String>();
+        uqks = new LinkedList<String>();
+        triggers = new LinkedList<String>();
+        indexs = new LinkedList<String[]>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LinkedList<String[]> getColumns() {
+        return columns;
+    }
+
+    public LinkedList<String> getPks() {
+        return pks;
+    }
+
+    public LinkedList<String> getFks() {
+        return fks;
+    }
+
+    public LinkedList<String> getUqks() {
+        return uqks;
+    }
+
+    public LinkedList<String> getTriggers() {
+        return triggers;
+    }
+
+    public LinkedList<String[]> getIndexs() {
+        return indexs;
+    }
+    
+    public void addColumn(String name, String type){
+        String[] col = new String[2];
+        col[0]=name;
+        col[1]=type;
+        columns.add(col);
+    }
+    
+    public void addIndex(String indexName, String unique, String columnName, String ascDesc){
+        String[] col = new String[4];
+        col[0]=indexName;
+        col[1]=unique;
+        col[2]=columnName;
+        col[3]=ascDesc;
+        columns.add(col);
+    }
+    
+    public void addTrigger(String name, String condition){
+        String[] col = new String[2];
+        col[0]=name;
+        col[1]=condition;
+        columns.add(col);
+    }
+    
+}
